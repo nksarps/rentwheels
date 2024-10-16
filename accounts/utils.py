6a40e9810676d330dc96c91e9537c1dc06e9693b
kweku-xvi <nksarps@gmail.com>
2024-10-16 17:09:36 +0000

    added email verification feature

---
+++ accounts/utils.py
@@ -0,0 +1,29 @@
+import os, random, string
+from dotenv import load_dotenv
+from trycourier import Courier
+
+load_dotenv()
+
+client = Courier(auth_token=os.getenv('COURIER_TOKEN'))
+
+def verify_account_mail(email:str, first_name:str, link:str):
+    client.send_message(
+        message={
+            "to": {
+            "email": email,
+            },
+            "template": os.getenv('VERIFICATION_MAIL_TEMPLATE_ID'),
+            "data": {
+            "appName": "RentWheels",
+            "firstName": first_name,
+            "link": link,
+            },
+        }
+    )
+
+
+def generate_id(n:int):
+    characters = string.ascii_letters + string.digits
+    id_generated = ''.join(random.choices(characters, k=n))
+
+    return id_generated
