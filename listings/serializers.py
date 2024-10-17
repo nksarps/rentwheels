98442b7643893ee20cb1d72022341db67860a619
kweku-xvi <nksarps@gmail.com>
2024-10-17 12:46:18 +0000

    added listing serializer

---
+++ listings/serializers.py
@@ -0,0 +1,10 @@
+from .models import Listing
+from rest_framework import serializers
+
+
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = ['id', 'image', 'make', 'model', 'year', 'body_type', 'fuel_type', 'transmission', 'status', 'seats', 'doors', 'currency', 'price_per_day']
+
+        read_only_fields = ['id']
