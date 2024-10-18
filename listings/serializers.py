5a605a7a51858e40dd567a187797342f8cc17f3c
kweku-xvi <nksarps@gmail.com>
2024-10-18 15:51:45 +0000

    [refactored listing serializer]

--- listings/serializers.py
+++ listings/serializers.py
@@ -5,6 +5,6 @@ from rest_framework import serializers
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
-        fields = ['id', 'image', 'make', 'model', 'year', 'body_type', 'fuel_type', 'transmission', 'status', 'seats', 'doors', 'currency', 'price_per_day']
+        fields = ['id', 'make', 'model', 'year', 'body_type', 'fuel_type', 'transmission', 'status', 'seats', 'doors', 'currency', 'price_per_day']
 
         read_only_fields = ['id']
