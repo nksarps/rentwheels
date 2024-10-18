ff5677e2c904100f2fd78ae733253deef2603197
kweku-xvi <nksarps@gmail.com>
2024-10-18 15:49:38 +0000

    added emails for payment confirmations and refund requests

--- accounts/utils.py
+++ accounts/utils.py
@@ -38,6 +38,48 @@ def password_reset_mail(email:str, first_name:str, link:str):
     )
 
 
+
+
+def payment_confirmation_mail(email, first_name, currency, amount, make, model, year, paid_at, booking_id, start_date, end_date, transaction_id):
+    client.send_message(
+        message={
+            "to": {
+            "email": email,
+            },
+            "template": os.getenv('PAYMENT_CONFIRMED_TEMPLATE_ID'),
+            "data": {
+            "firstName": first_name,
+            "currency": currency,
+            "amount": amount,
+            "make": make,
+            "model": model,
+            "year": year,
+            "paidAt": paid_at,
+            "bookingID": booking_id,
+            "startDate": start_date,
+            "endDate": end_date,
+            "transactionID": transaction_id,
+            },
+        }
+    )
+
+
+def refund_request_mail(email, first_name, booking_id):
+    client.send_message(
+        message={
+            "to": {
+            "email": email,
+            },
+            "template": os.getenv('REFUND_REQUEST_TEMPLATE_ID'),
+            "data": {
+            "companyName": "RentWheels",
+            "firstName": first_name,
+            "booking_id": booking_id
+            },
+        }
+    )
+
+
 def generate_id(n:int):
     characters = string.ascii_letters + string.digits
     id_generated = ''.join(random.choices(characters, k=n))
