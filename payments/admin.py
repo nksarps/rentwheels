35c2db19f6ccff07d54bc635ff7fc991c4d12b73
kweku-xvi <nksarps@gmail.com>
2024-10-18 15:57:29 +0000

    added payments to admin page

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,8 @@
+from .models import Payment
 from django.contrib import admin
 
-# Register your models here.
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'booking', 'user', 'amount', 'paid_at')
+    
+admin.site.register(Payment, PaymentAdmin)
